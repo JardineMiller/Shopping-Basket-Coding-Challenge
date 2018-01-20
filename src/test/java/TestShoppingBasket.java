cd577b5135414886bf51b3dd52c1c396c14af933
Jardine <jardine.miller@gmail.com>
2018-01-20 20:14:19 +0000

    can get basket value

--- src/test/java/TestShoppingBasket.java
+++ src/test/java/TestShoppingBasket.java
@@ -13,8 +13,8 @@ public class TestShoppingBasket {
     @Before
     public void before() {
         basket = new ShoppingBasket();
-        item = new Item("pizza", 50);
-        item2 = new Item("apple", 25);
+        item = new Item("pizza", 50.00);
+        item2 = new Item("apple", 25.00);
         basket.scan(item);
         basket.scan(item2);
     }
@@ -28,5 +28,10 @@ public class TestShoppingBasket {
     public void canRemoveItems() {
         basket.remove(item2);
         assertEquals(1, basket.count());
+    }
+
+    @Test
+    public void canGetBasketValue() {
+        assertEquals(75.00, basket.total(), 0.01);
     }
 }
